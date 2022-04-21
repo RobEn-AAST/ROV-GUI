f8b6ea925f6cec57f1fbc9804fbf6483531942d4
MahmoudHanyFathalla <87129311+MahmoudHanyFathalla@users.noreply.github.com>
2022-04-21 19:14:51 +0200

    Update new_members_GUI.py

--- new_members_GUI.py
+++ new_members_GUI.py
@@ -1,16 +1,49 @@
+from distutils.cmd import Command
 from pickle import TRUE
 from tkinter import Button, Tk, Label, X, Frame, Y, LEFT, BOTH
 import cv2
+from tkinter import *
 from PIL import Image, ImageTk
+
+   
+import sys
+import re
 root = Tk()
 
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
 root.geometry("%dx%d" % (width, height))
 root.title("ROV")
-# Initialize frames
 
+root.attributes('-fullscreen', True)
 
+def touch_1(n,width,height):
+    dim = (width,height)
+    new_window= Toplevel(root)
+    new_window.title("camera " + str(n))
+    new_window.geometry("%dx%d" % (width, height))
+
+    f3 = Frame(new_window, bg="pink")    
+    f3.pack(fill=BOTH, expand=True)
+    w4 = Label(f3, text="Blue", bg="blue", fg="white")
+    w4.pack(side=LEFT, fill=BOTH, expand=True)
+    
+   
+    while True:
+            
+        
+        ret,cameras_frame = camera_caps.read()
+        cv2image= cv2.cvtColor(cameras_frame,cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(cv2image)
+       
+        imgtk = ImageTk.PhotoImage(image = img)
+        w4.imgtk = imgtk
+        w4.configure(image=imgtk)
+        root.update_idletasks()
+        root.update()
+  
+def close():
+     root.destroy()   
 
 f1 = Frame(root, bg="grey")
 f2 = Frame(root, bg="pink")
@@ -19,10 +52,10 @@ f2 = Frame(root, bg="pink")
 w1 = Label(f1, text="Red", bg="black", fg="white",height=3)
 w2 = Label(f1, text="Green", bg="green", fg="white")
 
-b1 = Button(w1,text = "Button 1",width=15)
-b2 = Button(w1,text = "Button 2",width=15)
-b3 = Button(w1,text = "Button 3",width=15)
-b4 = Button(w1,text = "Button 4",width=15)
+b1 = Button(w1,text = "Button 1",width=15,command=lambda:touch_1(1,width,height))
+b2 = Button(w1,text = "Button 2",width=15,command=lambda:touch_1(1,width,height))
+b3 = Button(w1,text = "Button 3",width=15,command=lambda:touch_1(1,width,height))
+b4 = Button(w1,text = "quit",width=15,command=close)
 
 w1b = Label(f2, text="Red", bg="red", fg="white")
 w3b = Label(f2, text="Blue", bg="blue", fg="white")
@@ -46,12 +79,18 @@ w1b.pack(side=LEFT, fill=BOTH, expand=True)
 w3b.pack(side=LEFT, fill=BOTH, expand=True)
 
 camera_caps =cv2.VideoCapture(0)
+
+
 camera_caps.set(4,400)
+
 while True:
     ret,cameras_frame = camera_caps.read()
     cv2image= cv2.cvtColor(cameras_frame,cv2.COLOR_BGR2RGB)
     img = Image.fromarray(cv2image)
+    
     imgtk = ImageTk.PhotoImage(image = img)
+    
+   
     w3b.imgtk = imgtk
     w3b.configure(image=imgtk)
     w1b.imgtk = imgtk
