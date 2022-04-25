21d91341e931323107c4509658a24543f54c3d7c
omar9991 <omar85zaki@gmail.com>
2022-04-26 00:19:32 +0200

    Using deque instead of queue foe better performance

--- ROV_GUI.py
+++ ROV_GUI.py
@@ -31,29 +31,30 @@ def touch_1(n,width,height,index):
     w4 = Label(f3, text="Blue", bg="blue", fg="white")
     w4.pack(side=LEFT, fill=BOTH, expand=True)
     while True:
-        frame ,source  = camera_queue.get()
-        if source ==index:
-            cv2image= cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-            img = cv2.resize(cv2image, dim,fx = 2, fy = 2, interpolation = cv2.INTER_AREA)
-            img = Image.fromarray(img)
-            imgtk = ImageTk.PhotoImage(image = img)
-            w4.imgtk = imgtk
-            w4.configure(image=imgtk)
-            root.update_idletasks()
-            root.update()
+        if len(camera_queue)!= 0: 
+            frame ,source  = camera_queue.popleft()
+            if source ==index:
+                cv2image= cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                img = cv2.resize(cv2image, dim,fx = 2, fy = 2, interpolation = cv2.INTER_AREA)
+                img = Image.fromarray(img)
+                imgtk = ImageTk.PhotoImage(image = img)
+                w4.imgtk = imgtk
+                w4.configure(image=imgtk)
+                root.update_idletasks()
+                root.update()
         
   
 def close():
      root.destroy()  
 
-camera_queue = queue.Queue(maxsize=2000)
+camera_queue = deque()
 def camera_reader(source, camera_queue):
     print("Cam Loading...")
     cap = cv2.VideoCapture(source)
     print("Cam Loaded...")
     while(True):
         ret,frame = cap.read()
-        camera_queue.put([frame,source]) 
+        camera_queue.append([frame,source]) 
 
 camera_read = threading.Thread(target=camera_reader, args=(0, camera_queue,))
 camera_read.daemon = True
@@ -67,24 +68,17 @@ camera_read.start()
 # camera_read.daemon = True
 # camera_read.start() 
 
-# camera_read = threading.Thread(target=camera_reader, args=(0, camera_queue,))
-# camera_read.daemon = True
-# camera_read.start()  
 
-# camera_read = threading.Thread(target=camera_reader, args=(0, camera_queue,))
-# camera_read.daemon = True
-# camera_read.start()  
-
-camera_queue.join()     
+  
 
 f1 = Frame(root, bg="grey")
 f2 = Frame(root, bg="pink")
 
 # Initialize labels
 labels = (Label(f1, text="Red", bg="black", fg="white",height=3),
-          Label(f1, text="Green", bg="green", fg="white")
-          ,Label(f2, text="Red", bg="red", fg="white")
-         , Label(f2, text="Blue", bg="blue", fg="white") )
+          Label(f1, text="Green", bg="white", fg="white")
+          ,Label(f2, text="Red", bg="white", fg="white")
+         , Label(f2, text="Blue", bg="white", fg="white") )
 
 
 b1 = Button(labels[0],text = "Button 1",width=15,command=lambda:touch_1(1,width,height,0))
@@ -110,28 +104,27 @@ labels[2].pack(side=LEFT, fill=BOTH, expand=True)
 
 labels[3].pack(side=LEFT, fill=BOTH, expand=True)
 
-# camera_caps =cv2.VideoCapture(0)
-# camera_caps.set(4,400)
-# dim = (width,height)
+
 dim1 = (width//2,height//2)
 
 
-dim = [(width//2,height//2),(width,height//2),(width//2,height//2)]
+dim = [(width//2,height//2),(width//2,height//2),(width//2,height//2)]
     
 
 while True:
-    frame ,source  = camera_queue.get()
-    cv2image= cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    img1 = cv2.resize(cv2image,dim1,fx=1,fy=1, interpolation = cv2.INTER_AREA)
-   
-    img1 = Image.fromarray(img1)
+    if len(camera_queue)!=0:
+        frame ,source  = camera_queue.popleft()
+        cv2image= cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        img1 = cv2.resize(cv2image,dim[source+1],fx=1,fy=1, interpolation = cv2.INTER_AREA)
     
-    #img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    imgtk = ImageTk.PhotoImage(image = img1)
-    
-    labels[source+1].imgtk = imgtk
-    labels[source+1].configure(image=imgtk)
-    
+        img1 = Image.fromarray(img1)
+        
+        #img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        imgtk = ImageTk.PhotoImage(image = img1)
+        
+        labels[source+1].imgtk = imgtk
+        labels[source+1].configure(image=imgtk)
+        
 
-    root.update_idletasks()
-    root.update()
+        root.update_idletasks()
+        root.update()
