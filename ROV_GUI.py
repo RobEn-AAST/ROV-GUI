d45ec3eb3630e3439e3c6f6dcb3458996e5ee85c
omar9991 <omar85zaki@gmail.com>
2022-04-28 20:47:49 +0200

    some GUI enhancement

--- ROV_GUI.py
+++ ROV_GUI.py
@@ -10,6 +10,7 @@ root = Tk()
 import queue
 import threading
 from collections import deque
+import random
 
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
@@ -20,6 +21,18 @@ root.title("ROV")
 
 # Initialize frames
 root.attributes('-fullscreen', True)
+
+f1 = Frame(root)
+f2 = Frame(root)
+
+
+
+# f1.pack(fill=X)
+# f2.pack(fill=BOTH, expand=True)
+
+
+
+
 def touch_1(n,width,height,index):
     dim = (width,height)
     new_window = Toplevel(root)
@@ -60,9 +73,9 @@ camera_read = threading.Thread(target=camera_reader, args=(0, camera_queue,))
 camera_read.daemon = True
 camera_read.start() 
 
-camera_read = threading.Thread(target=camera_reader, args=(1, camera_queue,))
-camera_read.daemon = True
-camera_read.start() 
+# camera_read = threading.Thread(target=camera_reader, args=(1, camera_queue,))
+# camera_read.daemon = True
+# camera_read.start() 
 
 # camera_read = threading.Thread(target=camera_reader, args=(2, camera_queue,))
 # camera_read.daemon = True
@@ -74,31 +87,62 @@ camera_read.start()
 f1 = Frame(root, bg="grey")
 f2 = Frame(root, bg="pink")
 
+
+arr_image=['6.jpeg','7.jpeg','8.jpeg','9.jpeg','10.jpeg','11.jpeg','12.jpeg','13.jpeg','14.jpeg','15.jpeg','16.jpeg','17.jpeg','18.jpeg','19.jpeg','20.jpeg',]
+photos=random.sample(arr_image,2)
+photo1=photos[0]
+photo2=photos[1]
+img_right=Image.open(photo2)
+img_left=Image.open(photo1)
+img_right=img_right.resize((width//4,height//2))
+img_left=img_left.resize((width//4,height//2))
+img_right=ImageTk.PhotoImage(img_right)
+img_left=ImageTk.PhotoImage(img_left)
+
+photo_left=Label(f1,bg="white", image=img_left)
+photo_right=Label(f1,bg="white", image=img_right)
+
+
+
 # Initialize labels
 labels = (Label(f1, text="Red", bg="black", fg="white",height=3),
-          Label(f1, text="Green", bg="white", fg="white")
-          ,Label(f2, text="Red", bg="white", fg="white")
-         , Label(f2, text="Blue", bg="white", fg="white") )
+          Button(f1, text="", bg="white", fg="black",relief="sunken",command=lambda:touch_1(2,width,height,0))
+          ,Button(f2, text="", bg="white", fg="black",relief="sunken",command=lambda:touch_1(2,width,height,1))
+         , Button(f2, text="", bg="white", fg="black",relief="sunken",command=lambda:touch_1(2,width,height,2)) )
 
 
-b1 = Button(labels[0],text = "Button 1",width=15,command=lambda:touch_1(1,width,height,0))
-b2 = Button(labels[0],text = "Button 2",width=15,command=lambda:touch_1(1,width,height,1))
-b3 = Button(labels[0],text = "Button 3",width=15,command=lambda:touch_1(1,width,height,2))
-b4 = Button(labels[0],text = "quit",width=15,command=close)
+# b1 = Button(labels[0],text = "Button 1",width=15,command=lambda:touch_1(1,width,height,0))
+# b2 = Button(labels[0],text = "Button 2",width=15,command=lambda:touch_1(1,width,height,1))
+# b3 = Button(labels[0],text = "Button 3",width=15,command=lambda:touch_1(1,width,height,2))
+# b4 = Button(labels[0],text = "quit",width=15,command=close)
 
+buttom_exit = Button(labels[0],text = "X",bg="red",fg="white",font=3,relief="sunken",width=10,command=close)
 
 
 # Packing level 1
 f1.pack(fill=X)
 f2.pack(fill=BOTH, expand=True)
 
+
+
 # Packing level 2
 labels[0].pack(fill=X)
+photo_left.pack(side=LEFT, fill=BOTH, expand=True)
+photo_right.pack(side=RIGHT, fill=BOTH, expand=True)
+
+
+
+
 labels[1].pack(fill=X)
-b1.pack(side="left")
-b2.pack(side="left")
-b3.pack(side="left")
-b4.pack(side="left")
+
+
+
+# b1.pack(side="left")
+# b2.pack(side="left")
+# b3.pack(side="left")
+# b4.pack(side="left")
+
+buttom_exit.pack(side="right")
 
 labels[2].pack(side=LEFT, fill=BOTH, expand=True)
 
